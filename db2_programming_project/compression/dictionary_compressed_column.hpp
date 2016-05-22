0d6e84d3456f10ea84506ba39a7bebd2adb1e6c7
krorsok <erik.daenicke@st.ovgu.de>
2016-05-22 09:24:37 +0200

    Reformat code of Dictionary-Encoding

--- db2_programming_project/compression/dictionary_compressed_column.hpp
+++ db2_programming_project/compression/dictionary_compressed_column.hpp
@@ -8,45 +8,45 @@
 #include <core/compressed_column.hpp>
 
 namespace CoGaDB{
-	
+
 
 /*!
  *  \brief     This class represents a dictionary compressed column with type T, is the base class for all compressed typed column classes.
- */	
+ */
 template<class T>
 class DictionaryCompressedColumn : public CompressedColumn<T>{
-	public:
-	/***************** constructors and destructor *****************/
-	DictionaryCompressedColumn(const std::string& name, AttributeType db_type);
-	virtual ~DictionaryCompressedColumn();
+public:
+    /***************** constructors and destructor *****************/
+    DictionaryCompressedColumn(const std::string& name, AttributeType db_type);
+    virtual ~DictionaryCompressedColumn();
 
-	virtual bool insert(const boost::any& new_Value);
-	virtual bool insert(const T& new_value);
-	template <typename InputIterator>
-	bool insert(InputIterator first, InputIterator last);
+    virtual bool insert(const boost::any& new_Value);
+    virtual bool insert(const T& new_value);
+    template <typename InputIterator>
+    bool insert(InputIterator first, InputIterator last);
 
-	virtual bool update(TID tid, const boost::any& new_value);
-	virtual bool update(PositionListPtr tid, const boost::any& new_value);	
-	
-	virtual bool remove(TID tid);
-	//assumes tid list is sorted ascending
-	virtual bool remove(PositionListPtr tid);
-	virtual bool clearContent();
+    virtual bool update(TID tid, const boost::any& new_value);
+    virtual bool update(PositionListPtr tid, const boost::any& new_value);
 
-	virtual const boost::any get(TID tid);
-	//virtual const boost::any* const getRawData()=0;
-	virtual void print() const throw();
-	virtual size_t size() const throw();
-	virtual unsigned int getSizeinBytes() const throw();
+    virtual bool remove(TID tid);
+    //assumes tid list is sorted ascending
+    virtual bool remove(PositionListPtr tid);
+    virtual bool clearContent();
 
-	virtual const ColumnPtr copy() const;
+    virtual const boost::any get(TID tid);
+    //virtual const boost::any* const getRawData()=0;
+    virtual void print() const throw();
+    virtual size_t size() const throw();
+    virtual unsigned int getSizeinBytes() const throw();
 
-	virtual bool store(const std::string& path);
-	virtual bool load(const std::string& path);
+    virtual const ColumnPtr copy() const;
+
+    virtual bool store(const std::string& path);
+    virtual bool load(const std::string& path);
 
 
-	
-	virtual T& operator[](const int index);
+
+    virtual T& operator[](const int index);
 
     /*! values*/
     std::map<char, T> dictionary;
@@ -58,24 +58,24 @@ class DictionaryCompressedColumn : public CompressedColumn<T>{
 
 /***************** Start of Implementation Section ******************/
 
-	
-	template<class T>
+
+    template<class T>
     DictionaryCompressedColumn<T>::DictionaryCompressedColumn(const std::string& name, AttributeType db_type) : CompressedColumn<T>(name, db_type), dictionary(), columnEntries(), _name(name) {
 
     }
 
-	template<class T>
-	DictionaryCompressedColumn<T>::~DictionaryCompressedColumn(){
+    template<class T>
+    DictionaryCompressedColumn<T>::~DictionaryCompressedColumn(){
 
-	}
+    }
 
-	template<class T>
+    template<class T>
     bool DictionaryCompressedColumn<T>::insert(const boost::any&){
         // NOT NECESSARY FOR OUR PROGRAMMING TASK (NOT USED BY UNIT TEST).
         return false;
-	}
+    }
 
-	template<class T>
+    template<class T>
     bool DictionaryCompressedColumn<T>::insert(const T& new_value) {
         for(unsigned int i = 0; i < columnEntries.size(); i++) {
             if(dictionary.at(columnEntries[i]) == new_value) {
@@ -88,37 +88,37 @@ class DictionaryCompressedColumn : public CompressedColumn<T>{
         dictionary.insert(std::pair<char, T>(dictionary.size(), new_value));
 
         return true;
-	}
+    }
 
-	template <typename T> 
-	template <typename InputIterator>
+    template <typename T>
+    template <typename InputIterator>
     bool DictionaryCompressedColumn<T>::insert(InputIterator, InputIterator){
         // NOT NECESSARY FOR OUR PROGRAMMING TASK (NOT USED BY UNIT TEST).
-		return true;
-	}
+        return true;
+    }
 
-	template<class T>
+    template<class T>
     const boost::any DictionaryCompressedColumn<T>::get(TID){
         // NOT NECESSARY FOR OUR PROGRAMMING TASK (NOT USED BY UNIT TEST).
         return boost::any();
-	}
+    }
 
-	template<class T>
-	void DictionaryCompressedColumn<T>::print() const throw(){
+    template<class T>
+    void DictionaryCompressedColumn<T>::print() const throw(){
         // NOT NECESSARY FOR OUR PROGRAMMING TASK (NOT USED BY UNIT TEST).
-	}
+    }
 
-	template<class T>
-	size_t DictionaryCompressedColumn<T>::size() const throw(){
+    template<class T>
+    size_t DictionaryCompressedColumn<T>::size() const throw(){
         return columnEntries.size();
-	}
+    }
 
-	template<class T>
+    template<class T>
     const ColumnPtr DictionaryCompressedColumn<T>::copy() const{
         return ColumnPtr(new DictionaryCompressedColumn<T>(*this));
-	}
+    }
 
-	template<class T>
+    template<class T>
     bool DictionaryCompressedColumn<T>::update(TID tid, const boost::any& new_value){
         if(tid >= columnEntries.size()) {
             return false;
@@ -134,15 +134,15 @@ class DictionaryCompressedColumn : public CompressedColumn<T>{
         columnEntries[tid] = dictionary.size();
         dictionary.insert(std::pair<char, T>(dictionary.size(), boost::any_cast<T>(new_value)));
         return true;
-	}
+    }
 
-	template<class T>
+    template<class T>
     bool DictionaryCompressedColumn<T>::update(PositionListPtr, const boost::any&){
         // NOT NECESSARY FOR OUR PROGRAMMING TASK (NOT USED BY UNIT TEST).
         return false;
-	}
-	
-	template<class T>
+    }
+
+    template<class T>
     bool DictionaryCompressedColumn<T>::remove(TID tid){
         if(tid >= columnEntries.size()) {
             return false;
@@ -150,21 +150,21 @@ class DictionaryCompressedColumn : public CompressedColumn<T>{
 
         columnEntries.erase(columnEntries.begin() + tid);
         return true;
-	}
-	
-	template<class T>
-	bool DictionaryCompressedColumn<T>::remove(PositionListPtr){
-        // NOT NECESSARY FOR OUR PROGRAMMING TASK (NOT USED BY UNIT TEST).
-		return false;			
-	}
+    }
 
-	template<class T>
-	bool DictionaryCompressedColumn<T>::clearContent(){
+    template<class T>
+    bool DictionaryCompressedColumn<T>::remove(PositionListPtr){
+        // NOT NECESSARY FOR OUR PROGRAMMING TASK (NOT USED BY UNIT TEST).
+        return false;
+    }
+
+    template<class T>
+    bool DictionaryCompressedColumn<T>::clearContent(){
         columnEntries.clear();
         return true;
-	}
+    }
 
-	template<class T>
+    template<class T>
     bool DictionaryCompressedColumn<T>::store(const std::string& path_){
         // Recreate normal/standard column form?
 
@@ -193,9 +193,9 @@ class DictionaryCompressedColumn : public CompressedColumn<T>{
 //        }
 
         return true;
-	}
+    }
 
-	template<class T>
+    template<class T>
     bool DictionaryCompressedColumn<T>::load(const std::string& path_){
         std::string path(path_);
         path += _name;
@@ -204,22 +204,23 @@ class DictionaryCompressedColumn : public CompressedColumn<T>{
         boost::archive::binary_iarchive ia(infile);
 
 //        ia >> columnEntries;
+
         infile.close();
 
         return true;
-	}
+    }
 
-	template<class T>
+    template<class T>
     T& DictionaryCompressedColumn<T>::operator[](const int index){
         char key = columnEntries[index];
         return dictionary.at(key);
-	}
+    }
 
-	template<class T>
-	unsigned int DictionaryCompressedColumn<T>::getSizeinBytes() const throw(){
+    template<class T>
+    unsigned int DictionaryCompressedColumn<T>::getSizeinBytes() const throw(){
         // What shall be returned? Size of columnEntries and dictionary?
         return columnEntries.capacity() * sizeof(T);
-	}
+    }
 
 /***************** End of Implementation Section ******************/
 
